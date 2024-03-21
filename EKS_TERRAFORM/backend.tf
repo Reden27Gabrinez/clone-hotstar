649cc4b5d5f8e7da756aceb19db6f263978b7421
Reden Gabrinez <101633749+Reden27Gabrinez@users.noreply.github.com>
2024-03-22 01:44:20 +0800

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "bucket-prod-gabrinez-003" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
